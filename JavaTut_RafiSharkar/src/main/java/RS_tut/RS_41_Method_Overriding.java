cc4fc116e774bb3c4bcf1526a7bd9bcca1297ff5
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-06-19 15:31:40 +0600

    Method Overriding

---
+++ JavaTut_RafiSharkar/src/main/java/RS_tut/RS_41_Method_Overriding.java
@@ -0,0 +1,32 @@
+package RS_tut;
+
+class A{
+    public int a;
+    public int rafi(){
+        return 7;
+    }
+
+
+    public void meth2(){
+        System.out.println("I am method 2 of class A");
+    }
+}
+class B extends A{
+    @Override       // recommended
+    public void meth2(){
+        System.out.println("I am method 2 of class B");
+    }
+    public void meth3(){
+        System.out.println("I am method 3 of class B");
+    }
+}
+
+public class RS_41_Method_Overriding {
+    public static void main(String[] args) {
+        A a = new A();
+        a.meth2();
+
+        B b = new B();
+        b.meth2();
+    }
+}
