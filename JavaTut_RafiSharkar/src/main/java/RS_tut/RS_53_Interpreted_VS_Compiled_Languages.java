062ad0168b79304cea2f643b02aa1d7d2da7acb6
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-06-26 22:21:50 +0600

    Interpreted vs Compiled languages

---
+++ JavaTut_RafiSharkar/src/main/java/RS_tut/RS_53_Interpreted_VS_Compiled_Languages.java
@@ -0,0 +1,26 @@
+package RS_tut;
+
+/*
+Interpreter vs Compiler__
+==> Interpreter translates one statement at a time into machine code.   (line by line)
+==> Compiler scans the entire program and translates whole of it into machine code. (program by program)
+
+Interpreter                                                                                                        Compiler
+* One statement at a time.                                                                                  * Entire program at a time
+* Interpreter is needed everytime.(if you want to run any interpreted code like      * Once compiled it is not needed.(when c++ file compiled and generate a .exe file then .exe file don't need
+   python then you need interpreter)                                                                       any compiler like gcc for c/c++)
+* Partial execution if error(total code is 90 line and error at 67th line then           * No execution if an error occurs at anywhere in program.
+   interpreter run 1-66 code and throw error after 67 line)
+* Usually easy for programmer and run slower than compiler                                * Usually not as easy for programmer and run faster than interpreter Ex: C, C++
+   Ex: JavaScript, Python
+
+
+What about java?
+--> Java is hybrid (combination of both)
+ */
+
+public class RS_53_Interpreted_VS_Compiled_Languages {
+    public static void main(String[] args) {
+
+    }
+}
