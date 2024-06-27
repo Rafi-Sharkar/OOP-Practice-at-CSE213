cd1761051055602826fc79d19b7165e5e8622860
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-06-27 08:22:33 +0600

    Run Java with manually

---
+++ Run Java with manually/rs/Rafi.java
@@ -0,0 +1,15 @@
+package rs;
+/* 
+ write at terminal__
+ _>javac .\Rafi.java        // then create a Rafi.class file. and this Rafi.class is byte code whoes we can run anyOS
+
+ after packages_
+ _>javac -d . *.java        // make .class file acording it's packages
+*/
+
+public class Rafi {
+
+    public static void main(String[] args){
+        System.out.println("I am class Rafi's main method!");
+    }
+}
---
+++ Run Java with manually/rs/Rafi1.java
@@ -0,0 +1,12 @@
+package rs.com;
+/* 
+ write at terminal__
+ _>javac .\Rafi.java        // then create a Rafi.class file. and this Rafi.class is byte code whoes we can run anyOS
+*/
+
+public class Rafi1 {
+
+    public static void main(String[] args){
+        System.out.println("I am class Rafi's main method!");
+    }
+}
