3c8314b5ce5672ff509b3b28b1621fc3ebd7a904
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-06-27 08:21:25 +0600

    Complete packages at Run Java with manually folder

---
+++ JavaTut_RafiSharkar/src/main/java/RS_tut/RS_54_Packages_In_Java.java
@@ -0,0 +1,17 @@
+package RS_tut;
+
+/*
+    Packages in Java__
+    - A package is used to group related classes.
+     - Packages help in avoiding name conflicts.
+     - There are two types of packages
+        - Built_in packages --> Java API
+        - User defined packages --> Custom packages
+
+ */
+
+public class RS_54_Packages_In_Java {
+    public static void main(String[] args) {
+
+    }
+}
